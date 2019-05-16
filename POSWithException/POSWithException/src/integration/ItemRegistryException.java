2ce349ad2fa9e896892dfdcc5637ba053699a9ce
sadeq <sadeq@130.229.161.187>
2019-05-16 14:43:28 +0200

    exception

--- POSWithException/POSWithException/src/integration/ItemRegistryException.java
+++ POSWithException/POSWithException/src/integration/ItemRegistryException.java
@@ -5,7 +5,7 @@ package dbHandler;
  *Thrown when something goes wrong searching in database. Here while searching for a particular
  *  hardcoded, item identifier
  */
-public class ItemRegistryException extends Exception {
+public class ItemRegistryException extends RunTimeException {
 
 	private static final long serialVersionUID = -6272626728679110915L;
 	/**
